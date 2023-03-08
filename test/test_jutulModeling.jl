4e9b8011c36587c57afc52b4a82dc5cbaf28d3c2
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2023-03-08 13:22:50 -0500

    add test

--- test/test_jutulModeling.jl
+++ test/test_jutulModeling.jl
@@ -20,7 +20,20 @@ end
     states = S(x, q1)
     for i = 1:length(states.states)
         exist_co2 = sum(Saturations(states.states[i]) .* states.states[i].state[:PhaseMassDensities][1,:] .* model.ϕ) * prod(model.d)
-        inj_co2 = JutulDarcyAD.ρCO2 * q.irate * JutulDarcyAD.day * sum(tstep[1:i])
+        inj_co2 = JutulDarcyAD.ρCO2 * q.irate * JutulDarcyAD.day * sum(S.tstep[1:i])
         @test isapprox(exist_co2, inj_co2) rtol=1e-3
+    end
+end
+
+@testset "Test mass conservation for well modeling, different injection rates" begin
+    states = S(x, q)
+    pre_co2 = sum(Saturations(states.states[end]) .* states.states[end].state[:Reservoir][:PhaseMassDensities][1,:] .* model.ϕ) * prod(model.d)
+    q2 = jutulForce(q.irate * 0.5, q.loc)
+    S.tstep ./= 2
+    states_end = S(x, q2; state0=states)
+    for i = 1:length(states_end.states)
+        exist_co2 = sum(Saturations(states_end.states[i]) .* states_end.states[i].state[:Reservoir][:PhaseMassDensities][1,:] .* model.ϕ) * prod(model.d)
+        inj_co2 = JutulDarcyAD.ρCO2 * q2.irate * JutulDarcyAD.day * sum(S.tstep[1:i])
+        @test isapprox(exist_co2-pre_co2, inj_co2) rtol=1e-3
     end
 end
