b8f4bb058b8a75cbbba57ab5fc93f16a06973c82
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2023-03-14 19:40:45 -0400

    add porosity rrules

--- src/FlowRules/Operators/jutulModeling.jl
+++ src/FlowRules/Operators/jutulModeling.jl
@@ -8,7 +8,7 @@ end
 display(M::jutulModeling{D, T}) where {D, T} =
     println("$(D)D jutulModeling structure with $(sum(M.tstep)) days in $(length(M.tstep)) time steps")
 
-function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, f::Union{jutulForce{D, N}, jutulVWell{D, N}};
+function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, ϕ::AbstractVector{T}, f::Union{jutulForce{D, N}, jutulVWell{D, N}};
     state0=nothing, visCO2::T=T(visCO2), visH2O::T=T(visH2O),
     ρCO2::T=T(ρCO2), ρH2O::T=T(ρH2O), info_level::Int64=-1) where {D, T, N}
 
@@ -20,7 +20,9 @@ function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, f::U
     ### set up simulation configurations
     model, parameters, state0_, forces = setup_well_model(S.model, f, tstep; visCO2=visCO2, visH2O=visH2O, ρCO2=ρCO2, ρH2O=ρH2O)
     model.models.Reservoir.domain.grid.trans .= Transmissibilities
+    model.models.Reservoir.domain.grid.pore_volumes .= prod(S.model.d) .* ϕ
     parameters[:Reservoir][:Transmissibilities] = Transmissibilities
+    parameters[:Reservoir][:FluidVolume] .= prod(S.model.d) .* ϕ
 
     isnothing(state0) || (state0_[:Reservoir] = get_Reservoir_state(state0))
 
@@ -31,8 +33,8 @@ function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, f::U
     return output
 end
 
-function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, f::jutulSource{D, N};
-    state0::jutulSimpleState{T}=jutulSimpleState(S.model), visCO2::T=T(visCO2), visH2O::T=T(visH2O),
+function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, ϕ::AbstractVector{T}, f::jutulSource{D, N};
+    state0=nothing, visCO2::T=T(visCO2), visH2O::T=T(visH2O),
     ρCO2::T=T(ρCO2), ρH2O::T=T(ρH2O), info_level::Int64=-1) where {D, T, N}
 
     Transmissibilities = exp.(LogTransmissibilities)
@@ -43,7 +45,26 @@ function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, f::j
     tstep = day * S.tstep
     model = simple_model(S.model; ρCO2=ρCO2, ρH2O=ρH2O)
     model.domain.grid.trans .= Transmissibilities
+    model.domain.grid.pore_volumes .= prod(S.model.d) .* ϕ
     parameters = setup_parameters(model, PhaseViscosities = [visCO2, visH2O]);
-    states, _ = simulate(dict(state0), model, tstep, parameters = parameters, forces = forces, info_level = info_level, max_timestep_cuts = 1000)
+    state0_ = jutulSimpleState(S.model)
+    isnothing(state0) || (state0_ = state0)
+    states, _ = simulate(dict(state0_), model, tstep, parameters = parameters, forces = forces, info_level = info_level, max_timestep_cuts = 1000)
     return jutulSimpleStates(states)
+end
+
+function (S::jutulModeling{D, T})(f::Union{jutulForce{D, N}, jutulVWell{D, N}, jutulSource{D, N}};
+    LogTransmissibilities::AbstractVector{T}=KtoTrans(CartesianMesh(S.model), S.model.K), ϕ::AbstractVector{T}=S.model.ϕ,
+    state0=nothing, visCO2::T=T(visCO2), visH2O::T=T(visH2O),
+    ρCO2::T=T(ρCO2), ρH2O::T=T(ρH2O), info_level::Int64=-1) where {D, T, N}
+
+    return S(LogTransmissibilities, ϕ, f; state0=state0, visCO2=visCO2, visH2O=visH2O, ρCO2=ρCO2, ρH2O=ρH2O, info_level=info_level)
+end
+
+function (S::jutulModeling{D, T})(LogTransmissibilities::AbstractVector{T}, f::Union{jutulForce{D, N}, jutulVWell{D, N}, jutulSource{D, N}};
+    ϕ::AbstractVector{T}=S.model.ϕ,
+    state0=nothing, visCO2::T=T(visCO2), visH2O::T=T(visH2O),
+    ρCO2::T=T(ρCO2), ρH2O::T=T(ρH2O), info_level::Int64=-1) where {D, T, N}
+
+    return S(LogTransmissibilities, ϕ, f; state0=state0, visCO2=visCO2, visH2O=visH2O, ρCO2=ρCO2, ρH2O=ρH2O, info_level=info_level)
 end
